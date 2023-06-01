e9a3465ddf48aec62984bafe9c549a9c916e6dab
VishDroid-dev <vk2345732@gmail.com>
2023-06-01 07:24:59 +0530

    push

--- core/models.py
+++ core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Subject(models.Model):
     name = models.CharField(max_length=255)
@@ -8,23 +7,23 @@ class Subject(models.Model):
     
     def __str__(self):
         return self.name
-    
-    
+
+
 class Tutor(models.Model):
     name = models.CharField(max_length=100)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name} -> {self.subject.name}"
-    
-    
+
+
 class Class(models.Model):
     name = models.CharField(max_length=100)
     year = models.CharField(max_length=20, default="2020-2024")
     sem = models.CharField(max_length=20)
     tutor = models.OneToOneField(Tutor, on_delete=models.CASCADE)
     teachers = models.ManyToManyField(Tutor, related_name="teachers")
-    
+
     def __str__(self):
         return self.name
 
@@ -36,15 +35,15 @@ class Student(models.Model):
     clg_adm = models.CharField(max_length=100, unique=True)
     ktu_adm = models.CharField(max_length=100, unique=True)
     cgpa = models.FloatField(default=0)
-    
-    
+
+
     def __str__(self):
         return f"{self.name}"
-    
+
     class Result(models.Model):
         subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
         tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
         grade = models.CharField(max_length=2)
-        
+
         def __str__(self) -> str:
             return f"{self.subject.name} -> {self.tutor.name} -> {self.grade}"
